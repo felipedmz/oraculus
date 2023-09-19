fd521e666ea38c2782c98e2b6c5161011487e91f
Felipe Maziero <flpmaziero@gmail.com>
2023-09-18 23:15:19 -0300

    docs(comments): next steps

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ minutos = 10 # 1 trade a cada 1 min
 simple_robot = SimpleRobot(api)
 simple_robot.train()
 simple_robot.execute(minutos) 
+
+""" Estrategia a ser seguida
+
+grupo_robot = GroupRobot(api)
+grupo_robot.calcula_preco()
+    # -- train() -> preco previsto / arima
+grupo_robot.calcula_p_value()
+    # -- train() -> quanto de certeza eu tenho do preco de cima / cnn
+grupo_robot.trade()
+    # self.preco +20
+    # self.p_value 96%
+    # if tenho bitcoin
+        # acao = buy
+    # else 
+        # nao existem fundos suficientes / stop
+"""
