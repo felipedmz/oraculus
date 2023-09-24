df494c117e8c2e23635ba1a8f6f96fe0a0f9902c
Felipe Maziero <flpmaziero@gmail.com>
2023-09-23 23:45:40 -0300

    chore(comments): cleaning code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.pycaret_robot import PycaretRobot
 
 # config gerais
 api = Client('dev')
-minutos = 1 # 1 trade a cada 1 min
+minutos = 1 # trade a cada 1 min
 
 """ Baseline
 simple_robot = SimpleRobot(api)
@@ -17,18 +17,3 @@ best_selector = PycaretRobot(api)
 #best_selector.train()
 best_selector.execute(minutos)
 
-""" Estrategia a ser seguida
-
-grupo_robot = GroupRobot(api)
-grupo_robot.calcula_preco()
-    # -- train() -> preco previsto / arima
-grupo_robot.calcula_p_value()
-    # -- train() -> quanto de certeza eu tenho do preco de cima / cnn
-grupo_robot.trade()
-    # self.preco +20
-    # self.p_value 96%
-    # if tenho bitcoin
-        # acao = buy
-    # else 
-        # nao existem fundos suficientes / stop
-"""
